346cad106c226f788dc461596f0e6237570c813f
Ernest0G <ernesto7gutierrez@gmail.com>
2021-11-13 10:24:50 -0800

    commit

---
+++ populateTables.py
@@ -0,0 +1,36 @@
+from server import Users,Teachers,Classes,Enrollment,Students,db
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Betty = Users('BBrown', 'BBrown123') 
+db.session.add(Betty) 
+
+John = Users('JStuart', 'JStuart123') 
+db.session.add(John) 
+
+Li = Users('LCheng', 'LCheng123') 
+db.session.add(Li) 
+
+Nancy = Users('NLittle', 'NLittle123') 
+db.session.add(Nancy) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+Jose = Users('JSantos', 'Jsantos123') 
+db.session.add(Jose) 
+
+db.session.commit() 

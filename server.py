481b57d32033a64d001abf88fe34b17579543b37
Jacob Gilmartin <Jgilmartin@ucmerced.edu>
2021-11-15 15:36:36 -0800

    Create Branch
    Focusing entirely on implementing login with a cut down version of the main branch

--- server.py
+++ server.py
@@ -1,87 +1,39 @@
-from enum import unique
-from flask import Flask, jsonify, request, render_template, redirect, url_for, session
-from flask_login.utils import login_required
+from flask import Flask
+from flask_login.login_manager import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import backref, relationship
-from flask_login import current_user, login_user, LoginManager
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_login import UserMixin, LoginManager, current_user
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///university.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SECRET_KEY'] = 'mysecret'
+
 db = SQLAlchemy(app)
+login = LoginManager(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-app.secret_key = 'secret-key'
-
-class Users(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    username = db.Column(db.String, unique=True, nullable=False) 
-    password = db.Column(db.String, unique=False, nullable=False)
-
-    def __repr__(self): 
-        return '<Users %r>' % self.title
-
-    def checkPassword(self,password):
-        return self.password == password
-'''
-class Students(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    name = db.Column(db.String, unique=False, nullable=False) 
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'),unique=True,nullable=False)
-    
-    user = db.relationship('Users',backref=db.backref('students',lazy=True))
-
-    def __repr__(self): 
-        return '<Students %r>' % self.title
-
-class Enrollment(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    class_id = db.Column(db.Integer, db.ForeignKey('Classes.id'),unique=True, nullable=False) 
-    student_id = db.Column(db.Integer, db.ForeignKey('Students.id'),unique=True, nullable=False)
-    grade = db.Column(db.Integer, unique=False, nullable=True)
-
-class Classes(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    courseName = db.Column(db.String, unique=True, nullable=False) 
-    teacher_id = db.Column(db.Integer, db.ForeignKey('Teachers.id'),unique=True, nullable=False) 
-    numberEnrolled = db.Column(db.Integer, unique=False, nullable=False) 
-    capacity = db.Column(db.Integer, unique=False, nullable=False)
-    time = db.Column(db.String, unique=False, nullable=False) 
-
-class Teachers(db.Model): 
-    id = db.Column(db.Integer, primary_key=True) 
-    name = db.Column(db.String, unique=False, nullable=False) 
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'),unique=True, nullable=False)
-    
-'''
+@login.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
 
-@app.route('/', endpoint='landing')
-def index():
-    return render_template('login.html')
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
 
-@app.route('/login', methods =['POST'], endpoint='login')
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+admin = Admin(app)
+admin.add_view(MyModelView(Users, db.session))
+
+@app.route('/login')
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('/index'))
-    user = Users.query.filter_by(username = request.json['username']).first()
-    if user is None or not user.check_password(request.json['password']):
-        return redirect(url_for('login'))
+    user = Users.query.get(1)
     login_user(user)
-    return redirect(url_for('index'))
-
-
-@app.route('/index', endpoint='index')
-@login_required
-def index():
-    return render_template('index.html')
-
-@login_manager.user_loader
-def load_user(id):
-    return Users.get(id)
-
+    return 'Logged in'
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True) 
